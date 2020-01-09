0a182d4710898d9022fa161b5b0bba051a32351d
Andrea <andrea.secman@yahoo.com>
2020-01-09 13:48:11 +0200

    first commit

---
+++ pulse/pulse/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {AngularFireDatabase, snapshotChanges} from '@angular/fire/database';
+import { Observable } from 'rxjs';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  BPM1: object;
+  BPM:any[];
+  
+  constructor(db: AngularFireDatabase) {
+    db.list('/BPM')
+    .snapshotChanges().subscribe(BPM => {
+      this.BPM = BPM;
+      console.log(this.BPM);
+    }); 
+
+  }
+
+  public getValue() {
+    return this.BPM;
+  }
+}
+
---
+++ pulse/pulse/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAaWI4_OaB2jLcXwYUiV5NJqPI0s7x6uYA",
+    authDomain: "pulse-992cc.firebaseapp.com",
+    databaseURL: "https://pulse-992cc.firebaseio.com",
+    projectId: "pulse-992cc",
+    storageBucket: "pulse-992cc.appspot.com",
+    messagingSenderId: "17568823495",
+    appId: "1:17568823495:web:1e187b3323cdf5eb6566e2",
+    measurementId: "G-2ML6XJP748"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
